b96667ad47efa8dc1d675e181b54bdee02ea0a34
sakibbot <businessidbysakib@gmail.com>
2025-05-13 22:05:40 +0600

    Update auto.js

--- scripts/commands/auto.js
+++ scripts/commands/auto.js
@@ -30,7 +30,7 @@ const data = await alldown(content);
     fs.writeFileSync(__dirname + "/cache/auto.mp4", Buffer.from(video, "utf-8"))
 
         return api.sendMessage({
-            body: `《TITLE》: ${title}`,
+            body: `Video downloaded by SAKIB`,
             attachment: fs.createReadStream(__dirname + "/cache/auto.mp4")
 
         }, event.threadID, event.messageID);
